6b46e8d957ecab579c7f041bb8e0c6656ee81ce1
Yaroslav-Bondar <yarobondmail@gmail.com>
2023-03-25 20:09:20 +0200

    feat: add slider

--- shelter/public/js/main.js
+++ shelter/public/js/main.js
@@ -1,1 +1,76 @@
-!function(){var o={656:function(o){o.exports=(o,n)=>n*o},54:function(o){o.exports=(o,n)=>n+o}},n={};function t(r){var e=n[r];if(void 0!==e)return e.exports;var s=n[r]={exports:{}};return o[r](s,s.exports,t),s.exports}!function(){const o=t(54),n=t(656);console.log(o(4,5)),console.log(n(5,5))}()}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval("const sum = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/sum.js\");\nconst multiple = __webpack_require__(/*! ./module/multiple.js */ \"./src/js/module/multiple.js\");\n\nconsole.log(sum(4, 5));\nconsole.log(multiple(5, 5));\n\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/multiple.js":
+/*!***********************************!*\
+  !*** ./src/js/module/multiple.js ***!
+  \***********************************/
+/***/ (function(module) {
+
+eval("module.exports = (a, b) => b * a;\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/multiple.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/sum.js":
+/*!******************************!*\
+  !*** ./src/js/module/sum.js ***!
+  \******************************/
+/***/ (function(module) {
+
+eval("module.exports = (a, b) => b + a;\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/sum.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	
+/******/ })()
+;
