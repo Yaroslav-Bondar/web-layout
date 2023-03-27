7a6c8b54e5104b87aff77cd6b29af143b20575fc
Yaroslav-Bondar <yarobondmail@gmail.com>
2023-03-27 17:58:50 +0300

    feat: add pets page

--- shelter/public/js/main.js
+++ shelter/public/js/main.js
@@ -15,7 +15,7 @@
   \************************/
 /***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
-eval("const sum = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/sum.js\");\nconst multiple = __webpack_require__(/*! ./module/multiple.js */ \"./src/js/module/multiple.js\");\n\nconsole.log(sum(4, 5));\nconsole.log(multiple(5, 5));\n\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/main.js?");
+eval("const sum = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/sum.js\");\nconst multiple = __webpack_require__(/*! ./module/multiple.js */ \"./src/js/module/multiple.js\");\nconst score = __webpack_require__(/*! ./module/score.js */ \"./src/js/module/score.js\");\n\n// console.log(sum(4, 5));\n// console.log(multiple(5, 5));\nscore()\n\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/main.js?");
 
 /***/ }),
 
@@ -26,6 +26,16 @@ eval("const sum = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/s
 /***/ (function(module) {
 
 eval("module.exports = (a, b) => b * a;\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/multiple.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/score.js":
+/*!********************************!*\
+  !*** ./src/js/module/score.js ***!
+  \********************************/
+/***/ (function(module) {
+
+eval("const main = {\n  'верстка страницы валидная': 4,\n  'логотип в хедере состоит из текстовых элементов': 1,\n  'страница содержит ровно один элемент <h1>': 1,\n  'добавлен favicon': 1,\n  'блок <header>': 5,\n  'блок Not only': 5,\n  'блок About': 5,\n  'блок Our Friends': 5,\n  'блок Help': 5,\n  'блок In addition': 5,\n  'блок <footer>':5,\n  'для позиционирования элементов блока Help использована сеточная верстка (flexbox или grid)': 2,\n  'при уменьшении масштаба страницы браузера или увеличении ширины страницы (>1280px) вёрстка размещается по центру, а не сдвигается в сторону и не растягивается по всей ширине': 2,\n  'фоновый цвет тянется на всю ширину страницы': 2,\n  'интерактивность элементов': 12,\n  'TOTAL': 60,\n}\nconst pets = {\n  'Проверка верстки': 7,\n  'Вёрстка соответствует макету': 15,\n  'Требования к css': 4,\n  'Интерактивность элементов': 14,\n  'TOTAL': 40,\n};\n\nmodule.exports = () => {\n  console.log('Main page');\n  console.table(main);\n  console.log('Pets page');\n  console.table(pets);\n  console.log('TOTAL: 100');\n}\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/score.js?");
 
 /***/ }),
 
--- shelter/src/js/main.js
+++ shelter/src/js/main.js
@@ -1,6 +1,8 @@
 const sum = require('./module/sum.js');
 const multiple = require('./module/multiple.js');
+const score = require('./module/score.js');
 
-console.log(sum(4, 5));
-console.log(multiple(5, 5));
+// console.log(sum(4, 5));
+// console.log(multiple(5, 5));
+score()
 
