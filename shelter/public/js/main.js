af8ef100b1aec21b516bf268d7a32ef44bb12cc4
Yaroslav-Bondar <yarobondmail@gmail.com>
2023-04-03 22:25:11 +0300

    feat: add responsive and adaptive design for the main page

--- shelter/public/js/main.js
+++ shelter/public/js/main.js
@@ -1,1 +1,86 @@
-!function(){var o={656:function(o){o.exports=(o,e)=>e*o},739:function(o){const e={"верстка страницы валидная":4,"логотип в хедере состоит из текстовых элементов":1,"страница содержит ровно один элемент <h1>":1,"добавлен favicon":1,"блок <header>":5,"блок Not only":5,"блок About":5,"блок Our Friends":5,"блок Help":5,"блок In addition":5,"блок <footer>":5,"для позиционирования элементов блока Help использована сеточная верстка (flexbox или grid)":2,"при уменьшении масштаба страницы браузера или увеличении ширины страницы (>1280px) вёрстка размещается по центру, а не сдвигается в сторону и не растягивается по всей ширине":2,"фоновый цвет тянется на всю ширину страницы":2,"интерактивность элементов":12,TOTAL:60},n={"Проверка верстки":7,"Вёрстка соответствует макету":15,"Требования к css":4,"Интерактивность элементов":14,TOTAL:40};o.exports=()=>{console.log("*** Scores ***"),console.log("Main page"),console.table(e),console.log("Pets page"),console.table(n),console.log("TOTAL: 100")}},54:function(o){o.exports=(o,e)=>e+o}},e={};function n(t){var r=e[t];if(void 0!==r)return r.exports;var s=e[t]={exports:{}};return o[t](s,s.exports,n),s.exports}n(54),n(656),n(739)()}();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval("const sum = __webpack_require__(/*! ./module/sum.js */ \"./src/js/module/sum.js\");\nconst multiple = __webpack_require__(/*! ./module/multiple.js */ \"./src/js/module/multiple.js\");\nconst score = __webpack_require__(/*! ./module/score.js */ \"./src/js/module/score.js\");\n\n// console.log(sum(4, 5));\n// console.log(multiple(5, 5));\nscore()\n\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/multiple.js":
+/*!***********************************!*\
+  !*** ./src/js/module/multiple.js ***!
+  \***********************************/
+/***/ (function(module) {
+
+eval("module.exports = (a, b) => b * a;\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/multiple.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/score.js":
+/*!********************************!*\
+  !*** ./src/js/module/score.js ***!
+  \********************************/
+/***/ (function(module) {
+
+eval("const main = {\n  'верстка страницы валидная': 4,\n  'логотип в хедере состоит из текстовых элементов': 1,\n  'страница содержит ровно один элемент <h1>': 1,\n  'добавлен favicon': 1,\n  'блок <header>': 5,\n  'блок Not only': 5,\n  'блок About': 5,\n  'блок Our Friends': 5,\n  'блок Help': 5,\n  'блок In addition': 5,\n  'блок <footer>':5,\n  'для позиционирования элементов блока Help использована сеточная верстка (flexbox или grid)': 2,\n  'при уменьшении масштаба страницы браузера или увеличении ширины страницы (>1280px) вёрстка размещается по центру, а не сдвигается в сторону и не растягивается по всей ширине': 2,\n  'фоновый цвет тянется на всю ширину страницы': 2,\n  'интерактивность элементов': 12,\n  'TOTAL': 60,\n}\nconst pets = {\n  'Проверка верстки': 7,\n  'Вёрстка соответствует макету': 15,\n  'Требования к css': 4,\n  'Интерактивность элементов': 14,\n  'TOTAL': 40,\n};\n\nmodule.exports = () => {\n  console.log('*** Scores ***');\n  console.log('Main page');\n  console.table(main);\n  console.log('Pets page');\n  console.table(pets);\n  console.log('TOTAL: 100');\n}\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/score.js?");
+
+/***/ }),
+
+/***/ "./src/js/module/sum.js":
+/*!******************************!*\
+  !*** ./src/js/module/sum.js ***!
+  \******************************/
+/***/ (function(module) {
+
+eval("module.exports = (a, b) => b + a;\n\n\n//# sourceURL=webpack://gulp_layout/./src/js/module/sum.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	
+/******/ })()
+;
